c167068ba48415cf41c6f76f2d85e1b8fa07bab6
Spiegel <Adrian.Spiegel@EXXETA.com>
2020-11-19 11:49:28 +0100

    Responsiveness: Declared CSS rules for max-400px screens, added a sandwich menu and a sidebar

--- Resources/script.js
+++ Resources/script.js
@@ -2,6 +2,8 @@ const faProjects = document.getElementById('faProjects');
 const faContact = document.getElementById('faContact');
 const navContact = document.getElementById('navContact');
 const navProjects = document.getElementById('navProjects');
+const sidebar = document.getElementById('sidebar');
+const sandwich = document.getElementById('sandwich__container');
 
 const rotateNeg90deg = event => {
     if (event.target.id === 'navProjects') {
@@ -19,6 +21,15 @@ const resetTransform = event => {
     }
 }
 
+const shiftLeft = event => {
+    
+    if (getComputedStyle(sidebar).left === '250px') {
+        sidebar.style.left = '10px';
+    } else if (getComputedStyle(sidebar).left === '10px') {
+        sidebar.style.left = '250px';
+    }
+}
+
 [navProjects, navContact].forEach(arrElement => arrElement.addEventListener('mouseover', rotateNeg90deg));
 [navProjects, navContact].forEach(arrElement => arrElement.addEventListener('mouseout', resetTransform));
-
+sandwich.addEventListener('click', shiftLeft);
