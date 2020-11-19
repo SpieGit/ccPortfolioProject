276977a950c9714402d539b5846b21e34062cca4
Spiegel <Adrian.Spiegel@EXXETA.com>
2020-11-19 17:55:19 +0100

    Contact card added, refined JS

--- Resources/script.js
+++ Resources/script.js
@@ -4,21 +4,26 @@ const navContact = document.getElementById('navContact');
 const navProjects = document.getElementById('navProjects');
 const sidebar = document.getElementById('sidebar');
 const sandwich = document.getElementById('sandwich__container');
-const faBars = document.querySelector('.fa-bars')
+const faBars = document.querySelector('.fa-bars');
+const header = document.querySelector('.header');
+const contact = document.querySelector('.contact__container');
+const contactLi = document.querySelector('.contact__container li');
 
-const rotateNeg90deg = event => {
-    if (event.target.id === 'navProjects') {
-        faProjects.style.transform = 'rotate(-90deg)';
-    } else if (event.target.id === 'navContact') {
-        faContact.style.transform = 'rotate(-90deg)';
-    }
-}
-
-const resetTransform = event => {
-    if (event.target.id === 'navProjects') {
-        faProjects.style.transform = '';
-    } else if (event.target.id === 'navContact') {
-        faContact.style.transform = '';
+const rotate90deg = event => {
+    if (event.target === navProjects) {
+        if (getComputedStyle(faProjects).transform !== 'none') {
+            faProjects.style.transform = '';    
+        } else {
+            faProjects.style.transform = 'rotate(-90deg)';
+        }
+    } else if (event.target == navContact) {
+        if (getComputedStyle(faContact).transform !== 'none') {
+            faContact.style.transform = '';
+            contact.style.display = 'none';
+        } else {
+            faContact.style.transform = 'rotate(-90deg)';
+            contact.style.display = 'block';
+        }
     }
 }
 
@@ -33,6 +38,5 @@ const shiftLeft = () => {
     }
 }
 
-[navProjects, navContact].forEach(arrElement => arrElement.addEventListener('mouseover', rotateNeg90deg));
-[navProjects, navContact].forEach(arrElement => arrElement.addEventListener('mouseout', resetTransform));
+[navProjects, navContact].forEach(arrElement => arrElement.addEventListener('click', rotate90deg));
 [sandwich, faBars].forEach(arrElement => arrElement.addEventListener('click', shiftLeft));
