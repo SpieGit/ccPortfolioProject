37a55e4fdcf61285c916c7b71d6f3f7d3d943e89
Spiegel <Adrian.Spiegel@EXXETA.com>
2020-11-19 15:00:20 +0100

    Added icons and backdrop filter to sidebar, changed positions of header and sidebar to fixed- will scrow with document

--- Resources/script.js
+++ Resources/script.js
@@ -4,6 +4,7 @@ const navContact = document.getElementById('navContact');
 const navProjects = document.getElementById('navProjects');
 const sidebar = document.getElementById('sidebar');
 const sandwich = document.getElementById('sandwich__container');
+const faBars = document.querySelector('.fa-bars')
 
 const rotateNeg90deg = event => {
     if (event.target.id === 'navProjects') {
@@ -21,15 +22,17 @@ const resetTransform = event => {
     }
 }
 
-const shiftLeft = event => {
-    
-    if (getComputedStyle(sidebar).left === '250px') {
-        sidebar.style.left = '10px';
-    } else if (getComputedStyle(sidebar).left === '10px') {
-        sidebar.style.left = '250px';
+const shiftLeft = () => {
+
+    if (getComputedStyle(sidebar).right === '-250px') {
+        sidebar.style.right = '-10px';
+        sandwich.style.backgroundColor = 'rgba(255,255,255,0)'
+    } else if (getComputedStyle(sidebar).right === '-10px') {
+        sidebar.style.right = '-250px';
+        sandwich.style.backgroundColor = ''
     }
 }
 
 [navProjects, navContact].forEach(arrElement => arrElement.addEventListener('mouseover', rotateNeg90deg));
 [navProjects, navContact].forEach(arrElement => arrElement.addEventListener('mouseout', resetTransform));
-sandwich.addEventListener('click', shiftLeft);
+[sandwich, faBars].forEach(arrElement => arrElement.addEventListener('click', shiftLeft));
